c35666cdb969dee61ee5f7979c027ba509e31e21
SiphiwePat <siphiwe.pat@gmail.com>
2023-02-28 15:55:15 -0800

    3-islower.c

--- 3-islower.c
+++ 3-islower.c
@@ -1,13 +1,15 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * _islower - a function that checks for lowercase character
- * @c: single letter input
- * Return: 1 if int c is lowercase, 0 if otherwise
+ *_islower - checks for a lowercase character
+ *@c: letter being tested
+ * Return: Always 0
  */
+
 int _islower(int c)
 {
-	if (c >= 'a' && c <= 'z')
+
+	if (c >= 97 && c <= 122)
 		return (1);
 	else
 		return (0);

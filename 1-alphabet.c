eb2e563fb28e1302fc9ecb414f1ac961405148b4
SiphiwePat <siphiwe.pat@gmail.com>
2023-02-28 15:16:53 -0800

    1-alphabet.c

--- 1-alphabet.c
+++ 1-alphabet.c
@@ -1,17 +1,21 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * print_alphabet - a function that prints the alphabet, in lowercase
+ * print_alphabet - check the code for ALX School students.
  *
- * Return: Always 0 (Succes)
+ * Return: Always 0.
  */
+
 void print_alphabet(void)
 {
-	int n;
+	char alp = 'a';
 
-	for (n = 'a'; n <= 'z'; n++)
+	while (alp <= 'z')
 	{
-		_putchar(n);
+		_putchar (alp);
+		alp++;
+
 	}
-	_putchar('\n');
+	_putchar ('\n');
+
 }

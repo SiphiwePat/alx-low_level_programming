fff59e915c401e716f8e91a54e5323ba12d7a46d
SiphiwePat <siphiwe.pat@gmail.com>
2023-03-04 13:09:34 -0800

    0-reset_to_98.c  main.h  README.md

--- main.h
+++ main.h
@@ -1,11 +1,5 @@
-MAIN.H file
-
-
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stdio.h>
-#include <unistd.h>
+#ifndef FILE_MAIN
+#define FILE_MAIN
 
 int _putchar(char c);
 void reset_to_98(int *n);
@@ -18,5 +12,6 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
 
 #endif

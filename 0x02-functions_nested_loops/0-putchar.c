318af021e1a1bc5656c694587d9df7b667ca4207
SiphiwePat <siphiwe.pat@gmail.com>
2023-03-26 03:17:27 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,26 +1,4 @@
 #include "main.h"
-<<<<<<< HEAD
-	/**
-	 * main - print _putchar
-	 * is written by mbah
-	 * Return: 0
-	 */
-
-
-	int main(void)
-	{
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
-	}
-=======
 /**
  * main - print _putchar
  * is written by mbah
@@ -40,4 +18,4 @@ _putchar('r');
 _putchar('\n');
 return (0);
 }
->>>>>>> 295d5dd5b770bf51955fa99842cc164e8e42857d
+

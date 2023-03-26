94994637999e49b85b11e82bed99971122b19272
SiphiwePat <siphiwe.pat@gmail.com>
2023-03-26 03:20:11 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,13 +1,13 @@
 #include "main.h"
 #include <unistd.h>
 /**
- * _putcher - writes the character c to stout
+ * _putchar - writes the character c to stdout
  * @c: The character to print
  *
  * Return: On success 1.
- * On error, -1 is returned, and errno is set apparently.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-int _putcher(char c)
+int _putchar(char c)
 {
 	return (write(1, &c, 1));
-}	
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef main_h
+#define main_h
 
-/*
- * File: main.h
- *
- * Desc: Header file containing declartions for all function
- */
-
-int _putchar(char c);
+int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -17,27 +10,9 @@ int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
-void time_teble(void);
-int add(int, int);
-void print_to_98(int n);
-void print_time_table(int n);
-=======
-#ifndef main_h
-#define main_h
-
-int _putchar(char);
-void print_alphabet(void);
-void orint_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int abs(int);
-int print_last_digit(int);
-void jack_baver(void);
 void times_table(void);
 int add(int, int);
-void  print_to_98(int n);
+void print_to_98(int n);
 void print_times_table(int n);
->>>>>>> 295d5dd5b770bf51955fa99842cc164e8e42857d
 
 #endif
